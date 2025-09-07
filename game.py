52105d6b4f5a597dd5b9f75b9a2db5b392a6c90a
caharok0 <zvagincevkiril@gmail.com>
2025-09-07 13:29:55 +0300

    add move to hero

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from direct.showbase.ShowBase import ShowBase
 from mapmanager import Mapmanager
+from hero import Hero
 
 class Game(ShowBase):
     def __init__(self):
@@ -7,5 +8,6 @@ class Game(ShowBase):
         base.camLens.setFov(90)
         land = Mapmanager()
         x, y = land.load_map('maps/my_map.txt')
+        self.hero = Hero((x // 2, y // 2, 1), land)
 game = Game()
 game.run()
